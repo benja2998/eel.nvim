0489a313f1144b391c7de98a0ab68ca2611c7789
benja2998 <benja2998@proton.me>
2026-08-16 16:18:30 -0400

    update

--- plugin/eel.lua
+++ plugin/eel.lua
@@ -1,4 +1,4 @@
--- emacs.nvim
+-- eel.nvim
 vim.cmd("startinsert")
 vim.cmd("set iskeyword-=_")
 
@@ -11,11 +11,53 @@ vim.api.nvim_create_autocmd({'ModeChanged'}, {
 })
 
 -- define functions.
+function eel__goto()
+   local opts = { prompt = 'Goto line: ', scope = 'buffer' }
+
+   vim.ui.input(opts, function(input)
+		   local count = tonumber(input)
+		   vim.cmd(':'..count)
+   end)
+end
+function eel__query_replace()
+   local opts = { prompt = 'Query replace: ', scope = 'buffer' }
+   local to = ""
+   local replace = ""
+
+   vim.ui.input(opts, function(input)
+		   to = input
+   end)
+
+   opts = { prompt = 'Query replace '..to..' with: ', scope = 'buffer' }
+   vim.ui.input(opts, function(input)
+		   replace = input
+   end)
+
+   vim.cmd(',$s/'..to..'/'..replace..'/g')
+end
+
 function eel__kill_to_end()
    local buffer = vim.api.nvim_get_current_buf()
    local r,c = unpack(vim.api.nvim_win_get_cursor(0))
    local line_len = string.len(vim.api.nvim_get_current_line())
+   if line_len == 0 then
+      vim.cmd('call feedkeys("\\<Del>")')
+   end
    vim.api.nvim_buf_set_text(buffer, r - 1, c, r - 1, line_len, {})
+end
+
+function eel_add_key(lhs, rhs)
+   assert(type(lhs) == "string")
+   assert(type(rhs) == "string")
+
+   vim.keymap.set({'i', 'v', 'n'}, '<C-c>'..lhs, rhs)
+end
+
+function eel_add_CX_key(lhs, rhs)
+   assert(type(lhs) == "string")
+   assert(type(rhs) == "string")
+
+   vim.keymap.set({'i', 'v', 'n'}, '<C-x>'..lhs, rhs)
 end
 
 -- define basic emacs keys.
@@ -43,10 +85,18 @@ vim.keymap.set('i', '<Esc>>', "<C-o>G<C-o>$")
 vim.keymap.set('i', '<C-k>', eel__kill_to_end)
 vim.keymap.set('i', '<C-x><C-s>', "<C-o>:w<CR>")
 vim.keymap.set('i', '<C-x>s', "<C-o>:wa<CR>")
-
---vim.keymap.set({'i', 'v', 'c'}, '<C-Space>', '<Esc>v')
---vim.keymap.set('i', '<C-y>', '<Esc>pi')
---vim.keymap.set('n', '<lt>', 'ggi')
---vim.keymap.set('v', '<M-w>', 'y')
---vim.keymap.set('n', '<gt>', 'Gi')
---vim.keymap.set('v', '<C-w>', 'd')
+vim.keymap.set('i', '<C-s>', "<C-o>/")
+vim.keymap.set('i', '<C-c>', "<Nop>")
+vim.keymap.set('i', '<C-x><C-c>', "<C-o>:q<CR>")
+vim.keymap.set({'i', 'v', 'c'}, '<C-Space>', '<Esc>v')
+vim.keymap.set('i', '<C-y>', '<C-o>p')
+vim.keymap.set('i', '<C-x><C-f>', '<C-o>:find ')
+vim.keymap.set('i', '<C-x>d', '<C-o>:Ex ')
+vim.keymap.set('i', '<C-x>u', '<C-o>u ')
+vim.keymap.set('i', '<C-m>_', '<C-o><C-r> ')
+vim.keymap.set('i', '<M-%>', eel__query_replace)
+vim.keymap.set('v', '<M-w>', 'y')
+vim.keymap.set('v', '<C-w>', 'd')
+vim.keymap.set('v', '<C-g>', '<Esc>')
+vim.keymap.set({'i', 'v', 'n'}, '<M-g><M-g>', eel__goto)
+vim.cmd("set clipboard+=unnamedplus")
